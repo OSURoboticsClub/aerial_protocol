c549d1a752594c6129e470d0e8f271e226c815d4
Kyle Cesare <kcesare@gmail.com>
2014-12-16 22:31:32 -0800

    Add log message.

--- include/messages.hpp
+++ include/messages.hpp
@@ -12,11 +12,19 @@ struct heartbeat_message_t {
   std::uint8_t seq;
 } __attribute__((packed));
 
+struct log_message_t {
+  enum { ID = 0x02 };
+
+  char data[255];
+};
+
 std::uint16_t length(int id) {
   // TODO(kyle): sizeof(empty struct) is 1 in C++...
   switch(id) {
     case heartbeat_message_t::ID:
       return sizeof(heartbeat_message_t);
+    case log_message_t::ID:
+      return sizeof(log_message_t);
   }
 
   return 0; // TODO(kyle): Return something more meaningful?
